bf73ac080f7c5f2ee2fc0040168b1f7476e163ed
LEGOSROCKDUDE86 <arriagabrandon231@gmail.com>
2019-12-09 09:57:48 -0800

    hehhhhh
    hehhhhh

--- temp.py
+++ temp.py
@@ -5,10 +5,29 @@ import json
 app = Flask(__name__)
 
 @app.route("/")
-def intro():
+def render_main():
     with open('skyscrapers.json') as skyscraper_data:
         skyscraperdata = json.load(skyscraper_data)
     return render_template('desc.html')
+
+@app.route("/p1")
+def render_data():
+    with open('skyscrapers.json') as skyscraper_data:
+        skyscraperdata = json.load(skyscraper_data)
+    if 'buildings' in request.args:
+        return render_template('data.html', buildings = get_building_options(skyscraperdata))
+
+
+def get_building_options(skyscraperdata):
+    buildings = []
+    print("Render")
+    for data in skyscraperdata:
+        if data["name"] not in buildings:
+            buildings.append(data["name"])
+    options = ""
+    for data in buildings:
+        options = options + Markup("<option value=\"" + data + "\">" + data + "</option>")
+    return options
 
 if __name__=="__main__":
     app.run(debug=True)
