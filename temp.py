424b06aef99fa0a7530642ca66778fab6cfa3996
LEGOSROCKDUDE86 <arriagabrandon231@gmail.com>
2019-12-12 10:14:09 -0800

    data
    data

--- temp.py
+++ temp.py
@@ -15,7 +15,10 @@ def render_data():
     with open('skyscrapers.json') as skyscraper_data:
         skyscraperdata = json.load(skyscraper_data)
     if 'buildings' in request.args:
-        return render_template('data.html', buildings = get_building_options(skyscraperdata))
+        return render_template('data.html', buildings = get_building_options(skyscraperdata),
+         height1 = height(request.args['buildings'], skyscraperdata),
+         floors1 = floors(request.args['buildings'], skyscraperdata),
+         material1 = material(request.args['buildings'], skyscraperdata))
     else:
         return render_template('data.html', buildings = get_building_options(skyscraperdata))
 
@@ -29,6 +32,29 @@ def get_building_options(skyscraperdata):
     for data in buildings:
         options = options + Markup("<option value=\"" + data + "\">" + data + "</option>")
     return options
+
+def height(buildings, skyscraperdata):
+    height = 0
+    for data in skyscraperdata:
+        if data["name"] == buildings:
+            height = height + data["statistics"]["height"]
+
+    return height
+
+def floors(buildings, skyscraperdata):
+    floors = 0
+    for data in skyscraperdata:
+        if data["name"] == buildings:
+            floors = floors + data["statistics"]["floors above"]
+
+    return floors
+
+def material(buildings, skyscraperdata):
+    material = ""
+    for data in skyscraperdata:
+        if data["name"] == buildings:
+            material = data["material"]
+    return material
 
 if __name__=="__main__":
     app.run(debug=True)
